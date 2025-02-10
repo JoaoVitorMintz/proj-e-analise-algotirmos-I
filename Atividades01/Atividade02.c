c82076705a8afdf13c96aa378db7080781bc9dd8
JoaoVitorMintz <joaomintzz@gmail.com>
2025-02-10 20:54:49 -0300

    Adicionando atividades 1 em linguagem C da matéria Projeto e Analise de algoritmos I.

---
+++ Atividades01/Atividade02.c
@@ -0,0 +1,9 @@
+/*
+Escreva uma função chamado ultimoDigito() que receba dois inteiros a e b, não negativos
+e devolve true se a e b tem os mesmos últimos dígitos. Por exemplo, 57 e 27, ambos têm
+os últimos dígitos iguais a 7. Utilize a biblioteca stdbool.h para devolver booleano.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
