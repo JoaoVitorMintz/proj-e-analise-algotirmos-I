54c8c5199852d54364eb2171569971237ba15e61
João Vitor G A Mintz <joaomintzz@gmail.com>
2025-02-19 10:40:09 -0300

    Atualizado exercicio do professor

--- atividadeDeSala/ex01.c
+++ atividadeDeSala/ex01.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 int strLen(char s[]);
+void Strcpy(char dst[], char org[]);
 
 int main() {
 
     char str[20] = "abcde";
+    char str1[20];
     printf("Tamanho da String %s: %d", str, strLen(str));
+
+    Strcpy(str1, str);
+
+    printf("Tamanho da String  %s: %d", str1, strLen(str1));
 
     return 0;
 
@@ -14,6 +20,15 @@ int main() {
 int strLen(char s[]) {
     
     for (int c = 0; s[c] != '\0'; i++) {
-        return c - 1;
+        return c;
     }
+}
+
+void Strcpy(char dst[], char org[]) {
+    int i = 0;
+    while (org[i] != '\0') {
+        dst[i] = org[i];
+        i++;
+    }
+    dst[i] = '\0';
 }
