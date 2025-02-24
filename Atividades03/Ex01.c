2f34c009244039b4574bd8fd40016889b7a57dd1
João Vitor G A Mintz <joaomintzz@gmail.com>
2025-02-24 11:56:44 +0000

    Atualizado ex01 corrigid

--- Atividades03/Ex01.c
+++ Atividades03/Ex01.c
@@ -1,80 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-int strcmp(char S1[], char S2[]);
-int strcmp2(char S2[], char S3[]);
-int strcmp3(char S1[], char S3[]);
+void encontreMaior(char s1[], char s2[], char s3[], char maior[]);
 
 int main() {
+    char s1[10] = "abc";
+    char s2[10] = "bcd";
+    char s3[10] = "cde";
+    char maior[10];
     
-    char v1[10] = {'L', 'o', 'r', 'e', 'm', '\0'};
-    char v2[10] = {'I', 'p', 's', 'u', 'm', '\0'};
-    char v3[10] = {'d', 'o', 'l', 'o', 'r', '\0'};
-    
-    printf("S1 = %s, S2 = %s, S3 = %s, ENTRE S1 e S2 = %d, ENTRE S2 e S3 = %d, ENTRE S1 e S3 = %d\n", v1, v2, v3, strcmp(v1, v2), strcmp2(v2, v3), strcmp3(v1, v3));
+    encontreMaior(s1, s2, s3, maior);
+    printf("A maior string em ordem lexicográfica: %s", maior);
     
     return 0;
-    
 }
 
-int strcmp(char S1[], char S2[]) {
+void encontreMaior(char s1[], char s2[], char s3[], char maior[]) {
+    int comparacao = strcmp(s1, s2);
     
-    int i = 0;
-    while(S1[i] != '\0' && S2[i] != '\0') {
-        if(S1[i] < S2[i]) {
-            return -1;
-        } else if( S1[i] > S2[i]) {
-            return 1;
-        }
-        i++;
-    }
-    if (S1[i] == '\0' && S2[i] == '\0') {
-        return 0;
-    } else if(S2[i] != '\0') {
-        return -1;
+    if (comparacao >= 0) {
+        strcpy(maior, s1);
     } else {
-        return 1;
+        strcpy(maior, s2);
     }
     
-}
-
-int strcmp2(char S2[], char S3[]) {
+    comparacao = strcmp(s3, maior);
     
-    int i = 0;
-    while(S2[i] != '\0' && S3[i] != '\0') {
-        if(S2[i] < S3[i]) {
-            return -1;
-        } else if( S2[i] > S3[i]) {
-            return 1;
-        }
-        i++;
+    if (comparacao > 0) {
+        strcpy(maior, s3);
     }
-    if (S2[i] == '\0' && S3[i] == '\0') {
-        return 0;
-    } else if(S3[i] != '\0') {
-        return -1;
-    } else {
-        return 1;
-    }
-    
-}
-
-int strcmp3(char S1[], char S3[]) {
-    
-    int i = 0;
-    while(S1[i] != '\0' && S3[i] != '\0') {
-        if(S1[i] < S3[i]) {
-            return -1;
-        } else if( S1[i] > S3[i]) {
-            return 1;
-        }
-        i++;
-    }
-    if (S1[i] == '\0' && S3[i] == '\0') {
-        return 0;
-    } else if(S3[i] != '\0') {
-        return -1;
-    } else {
-        return 1;
-    }
-    
 }
